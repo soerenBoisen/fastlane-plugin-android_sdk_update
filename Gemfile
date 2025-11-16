d59dc3b87e425f13f4fa738708ca914061817288
Søren Boisen <soeren.boisen@facilitynet.dk>
2025-11-16 23:56:11 +0100

    Add macOS support for gradle_update action

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
 source 'https://rubygems.org'
 
+# HACK: Needed until fastlane grows proper Ruby 3.4 support
+gem "abbrev"
+gem "mutex_m"
+gem "ostruct"
+gem "bigdecimal"
+gem "nkf"
+
 gemspec
 
 plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
--- lib/fastlane/plugin/android_sdk_update/actions/gradle_update_action.rb
+++ lib/fastlane/plugin/android_sdk_update/actions/gradle_update_action.rb
@@ -15,9 +15,29 @@ module Fastlane
         return gradle_path
       end
 
+      def self.get_download_tool(params)
+        case FastlaneCore::Helper.operating_system
+        when "macOS"
+          return params[:download_tool_macos]
+        when "linux"
+          return params[:download_tool_linux]
+        else
+          UI.user_error!("Operating system not supported: #{FastlaneCore::Helper.operating_system}")
+        end
+      end
+
+      def self.get_download_command(download_tool, download_url, output_path)
+        case download_tool
+        when "wget"
+          return "wget -O #{output_path} #{download_url}"
+        when "curl"
+          return "curl -L #{download_url} -o #{output_path}"
+        end
+      end
+
       def self.determine_gradle(params)
         # on linux
-        if FastlaneCore::Helper.linux?
+        if FastlaneCore::Helper.linux? || FastlaneCore::Helper.mac?
           gradle_version = params[:gradle_version]
           gradle_install_path = File.expand_path(params[:gradle_dir])
           gradle_path = File.expand_path("gradle-#{gradle_version}", gradle_install_path)
@@ -28,7 +48,7 @@ module Fastlane
           else
             UI.message("Downloading gradle to #{gradle_install_path}")
             download_url = "https://services.gradle.org/distributions/gradle-#{gradle_version}-bin.zip"
-            download_and_extract_gradle(download_url, gradle_version, gradle_install_path)
+            download_and_extract_gradle(params, download_url, gradle_version, gradle_install_path)
           end
 
         else
@@ -38,18 +58,22 @@ module Fastlane
         [gradle_sh, gradle_path]
       end
 
-      def self.download_and_extract_gradle(download_url, gradle_version, target_path)
-        FastlaneCore::CommandExecutor.execute(command: "wget -O /tmp/gradle-#{gradle_version}.zip #{download_url}",
+      def self.download_and_extract_gradle(params, download_url, gradle_version, target_path)
+        download_path = "/tmp/gradle-#{gradle_version}.zip"
+        download_tool = get_download_tool(params)
+        download_cmd = get_download_command(download_tool, download_url, download_path)
+
+        FastlaneCore::CommandExecutor.execute(command: download_cmd,
                                               print_all: true,
                                               print_command: true)
         FastlaneCore::CommandExecutor.execute(command: "mkdir -p #{target_path}",
                                               print_all: true,
                                               print_command: true)
-        FastlaneCore::CommandExecutor.execute(command: "unzip -qo /tmp/gradle-#{gradle_version}.zip -d #{target_path}",
+        FastlaneCore::CommandExecutor.execute(command: "unzip -qo #{download_path} -d #{target_path}",
                                               print_all: true,
                                               print_command: true)
       ensure
-        FastlaneCore::CommandExecutor.execute(command: "rm -f /tmp/gradle-#{gradle_version}.zip",
+        FastlaneCore::CommandExecutor.execute(command: "rm -f #{download_path}",
                                               print_all: true,
                                               print_command: true)
       end
@@ -87,6 +111,16 @@ module Fastlane
                                         description: "Directory for Gradle",
                                         optional: true,
                                         default_value: "~/.gradle-fastlane"),
+          FastlaneCore::ConfigItem.new(key: :download_tool_linux,
+                                        env_name: "FL_DOWNLOAD_TOOL_LINUX",
+                                        description: "Tool to download files in linux",
+                                        optional: true,
+                                        default_value: "wget"),
+          FastlaneCore::ConfigItem.new(key: :download_tool_macos,
+                                        env_name: "FL_DOWNLOAD_TOOL_MACOS",
+                                        description: "Tool to download files in macOS",
+                                        optional: true,
+                                        default_value: "curl"),
         ]
       end
 
